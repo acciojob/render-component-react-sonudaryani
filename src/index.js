928323b3b185a370b7de64dccf592efbf21eb982
sonudaryani <sonudaryani4@gmail.com>
2023-07-13 01:23:33 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,9 +3,5 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-function App() {
-  return ( 
-      <p>I am learning React. My life is getting better.</p> 
-    )
-}
+
 ReactDOM.render(<App />, document.getElementById("root"));
